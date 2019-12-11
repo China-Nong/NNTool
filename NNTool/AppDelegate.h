6dfd9016d5feb03cce66004bc2bda914ebea3521
China-nong <nn778899nn@163.com>
2019-12-11 09:09:33 +0800

    Initial Commit

---
+++ NNTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NNTool
+//
+//  Created by cycx on 2019/12/11.
+//  Copyright © 2019 cycx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NNTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NNTool
+//
+//  Created by cycx on 2019/12/11.
+//  Copyright © 2019 cycx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
